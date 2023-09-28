6f201d1b1190f33f57ad138e2c98eecd28aa95f9
Sammybett-bit <bettsammy20@gmail.com>
2023-09-28 08:21:43 +0300

    completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = ''
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    def is_question(self):
+        return self._value.endswith('?')
+
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+    def count_sentences(self):
+        sentences = self._value.split('.')
+        sentences = [s.strip() for s in sentences if s]
+        return len(sentences)
+
+if __name__ == "__main__":
+    my_string = MyString("Hello World.")
+    print(my_string.value)  
+    print(my_string.is_sentence())  
+    print(my_string.is_question())  
+    print(my_string.is_exclamation())  
+    print(my_string.count_sentences())  
